c88e341081b163b1e58d416a0695032a03ba92e2
2148811982 <2148811982@qq.com>
2019-02-21 23:21:39 +0800

    11

--- src/main/java/spittr/controller/SpitterController.java
+++ src/main/java/spittr/controller/SpitterController.java
@@ -1,7 +1,5 @@
 package spittr.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +7,11 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.method.annotation.ErrorsMethodArgumentResolver;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import spittr.domain.Spitter;
 import spittr.service.ISpitterService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("spitter")
@@ -34,19 +33,23 @@ public class SpitterController {
 	}
 	
 	@RequestMapping(value = "register", method = RequestMethod.POST)
-	public String processRegistration(@Valid Spitter spitter, Errors errors, Model model) {
+	public String processRegistration(@Valid Spitter spitter, Errors errors, RedirectAttributes model) {
 		if(errors.hasErrors())
 			return "registerForm";
 		spitterService.save(spitter);
 		model.addAttribute("username",spitter.getUsername());
-		model.addAttribute("spitterId",spitter.getId());
+//		model.addAttribute("spitterId",spitter.getId());
+		model.addFlashAttribute(spitter);
+
 		return "redirect:/spitter/{username}";
 	}
 	
 	@RequestMapping("{username}")
 	public String showSpitterProfile(@PathVariable String username, Model model) {
-		Spitter spitter = spitterService.findByUsername(username);
-		model.addAttribute("spitter", spitter);
+		if (!model.containsAttribute("spitter")){
+			Spitter spitter = spitterService.findByUsername(username);
+			model.addAttribute("spitter", spitter);
+		}
 		return "profile";
 	}
 }
